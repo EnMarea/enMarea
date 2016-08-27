aa060cf1918336c14dd373b44ba1facc4ca40734
oscarotero <oom@oscarotero.com>
2016-08-27 20:25:55 +0200

    correxido o listado de novas

--- templates/partials/news/list.php
+++ templates/partials/news/list.php
@@ -5,11 +5,13 @@ use Jenssegers\Date\Date;
 
 <li>
 	<article class="new is-list" data-folk="news,<?= $new->id ?>">
+		<?php if (!empty($new->imageFile)): ?>
 		<figure class="new-image">
 			<a href="<?= $this->url('new', ['slug' => $new->slug]) ?>">
 			<img src="<?= $this->img('uploads/news/imageFile/'.$new->imageFile->getFilename(), 'small.') ?>">
 			</a>
 		</figure>
+		<?php endif ?>
 		<div class="new-content">
 			<header>
 				<h2 class="new-title">
