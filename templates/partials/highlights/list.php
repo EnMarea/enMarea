2ce53edc94bcdf7e8fe2b624f59bc2e20130ec1d
oscarotero <oom@oscarotero.com>
2016-09-02 15:49:27 +0200

    colocado o tipo de destacado xunto ao título

--- templates/partials/highlights/list.php
+++ templates/partials/highlights/list.php
@@ -9,18 +9,17 @@
 	<?php else: ?>
 	<a href="<?= $highlight->url ?>" class="highlight-image">
 		<img src="<?= $this->img('uploads/highlights/imageFile/'.$highlight->imageFile->getFilename(), 'small.') ?>">
-
-		<p class="highlight-details">
-			<?php if (!empty($highlight->type)): ?>
-			<strong class="highlight-type"><?= $highlight->type ?></strong>
-			<?php endif ?>
-		</p>
 	</a>
 	<?php endif ?>
 
 	<?php if (!$highlight->showOnlyCode()): ?>
 	<a href="<?= $highlight->url ?>">
-		<h2 class="highlight-title"><?= $highlight->title ?></h2>
+		<h2 class="highlight-title">
+			<?php if (!empty($highlight->type)): ?>
+			<strong class="highlight-type"><?= $highlight->type ?></strong>
+			<?php endif ?>
+			<?= $highlight->title ?>
+		</h2>
 	</a>
 	<?php endif ?>
 </li>
