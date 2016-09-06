9c4e2e3cd53f52a8cba4c676252b16275368268d
oscarotero <oom@oscarotero.com>
2016-09-06 13:47:08 +0200

    caché de assets

--- app/Providers/Templates.php
+++ app/Providers/Templates.php
@@ -17,7 +17,7 @@ class Templates implements ServiceProviderInterface
             $templates->addData(['app' => $app]);
 
             $templates->registerFunction('asset', function () use ($app) {
-                return call_user_func_array([$app, 'getUrl'], func_get_args()).'?v11';
+                return call_user_func_array([$app, 'getUrl'], func_get_args()).'?v12';
             });
 
             $svg = Collection::fromPath($app->getPath('www/img/svg'));
